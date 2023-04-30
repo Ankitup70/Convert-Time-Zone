be60151fab58fec5d902b275e4dfcdffe1746146
ANKIT KUMAR BHARTIYA <132056649+Ankitup70@users.noreply.github.com>
2023-04-30 23:21:39 +0530

    Add files via upload

---
+++ ConvertTimeZone.java
@@ -0,0 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import java.util.*;
+
+class ConvertTimeZone {
+    public static void main(String[] args) {
+        {
+            String format = " [ dd-MM-yyyy ]  [ HH:mm:ss aa ]";
+            SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+            Date dt = new  Date();
+
+            String defaulTZ = TimeZone.getDefault().getID(); 
+            System.out.println(defaulTZ);
+            
+            Calendar cal = new GregorianCalendar();
+            cal.setTime(dt);
+
+            System.out.println("Date : " +dateFormat.format(cal.getTime()));
+            System.out.println("Timezone:" +cal.getTimeZone().getID());
+            System.out.println("Timezone Name: " +cal.getTimeZone().getDisplayName());
+
+            TimeZone jnbTZ = TimeZone.getTimeZone("Africa/Johannesburg");
+            SimpleDateFormat jnbFormat = new SimpleDateFormat(format);
+            jnbFormat.setTimeZone(jnbTZ);
+
+            cal.setTimeZone(jnbTZ);
+            
+            System.out.println("After conversion");
+            System.out.println("Date: " +jnbFormat.format(cal.getTime()) );
+            System.out.println("TimeZone: " +cal.getTimeZone().getID());
+            System.out.println("TimeZone Name: "+cal.getTimeZone().getDisplayName()  );
+        }
+    }
+
+}
